f669e91ea9ace102932cdf890394a808d99746a9
abhicalyptic <abhiyadv14@gmail.com>
2024-03-31 12:52:57 +0530

    Hurray !

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import requests
-for line in open("/Users/ASUS/Desktop/projects/celosia/sources/wallhaven/sources.txt","r").readlines():
+for line in open("C:\\Users\\abhiy\\Desktop\\gitGithub\\celosia\\sources\\wallhaven\\sources.txt").readlines():
     response=requests.get(line)
     if response.status_code != 200:
         print(line)
--- install.py
+++ install.py
@@ -2,7 +2,7 @@ import ctypes
 import os
 SPI_SETDESKWALLPAPER = 20 
 directory=os.getcwd()
-image="sample.png"
+image="celosia\sample.png"
 directory=directory+"/"+image
 print(directory)
 ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0,directory, 3) 
